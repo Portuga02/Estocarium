919ce128911fd938b24c21b250f37eeefb3cdb66
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2023-11-28 12:36:48 -0300

    ajustando front

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -35,6 +35,11 @@ export default function Authenticated({ user, header, children }) {
                                     Lista de itens do estoque
                                 </NavLink>
                             </div>
+                            <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                                <NavLink href={route('dashboard')} active={route().current('Estocarium')}>
+                                   Cadastra novo item
+                                </NavLink>
+                            </div>
                         </div>
 
                         <div className="hidden sm:flex sm:items-center sm:ms-6">
